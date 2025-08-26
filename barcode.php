1db21f718acc018553a4b1eb0c8de1e8dd03bc1d
Quixotic <146061426+ajisayfullah@users.noreply.github.com>
2025-08-26 07:24:02 +0700

    update 4
    menambahkan halaman barcode

---
+++ barcode.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Setor Barcode</title>
+  <link rel="stylesheet" href="resources/css/barcode.css">
+</head>
+<body>
+  <!-- Header -->
+  <div class="header">
+    <img src="resources/img/money_save.png" alt="Money Save" class="logo">
+  </div>
+
+  <!-- Konten -->
+  <div class="content">
+    <div class="barcode-box">
+      <img src="resources/img/barcode.png" alt="Barcode">
+    </div>
+
+    <div class="instruction">
+  <h3 class="title">Petunjuk & Arahan</h3>
+  <p>
+    Barcode ini berfungsi sebagai bukti setoran tabungan Anda.
+    Silakan tunjukkan barcode ini kepada petugas Bank Mini
+    agar dapat dipindai dan dicatat sebagai setoran resmi.
+    <span class="blue">Pastikan Anda membawa uang setoran sesuai nominal yang akan dibayarkan.</span>
+  </p>
+  <br>
+  <p>
+    <span class="note">Note:</span>
+    Jangan lupa untuk memeriksa menu Riwayat guna memastikan saldo setoran sudah tercatat atau belum.
+  </p>
+</div>
+
+
+  <!-- Footer -->
+  <div class="footer">
+      <div class="footer-item">
+          <img src="resources/icon/home_polos.png" alt="Home">
+          <p>Beranda</p>
+      </div>
+      <div class="footer-item">
+          <img src="resources/icon/riwayat.png" alt="Riwayat">
+          <p>Riwayat</p>
+      </div>
+      <div class="footer-item qr active">
+          <img src="resources/icon/money.png" alt="Wallet">
+      </div>
+      <div class="footer-item">
+          <img src="resources/icon/notif.png" alt="Notif">
+          <p>Notifikasi</p>
+      </div>
+      <div class="footer-item">
+          <img src="resources/icon/profile.png" alt="Profile">
+          <p>Profile</p>
+      </div>
+  </div>
+</body>
+</html>
